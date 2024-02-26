9fc90667c99cecbf5ada4ba33e6baf226f8543a4
njordulv <njordulv@gmail.com>
2024-02-26 19:03:34 +0200

    ADD: favicons, font

--- App.tsx
+++ App.tsx
@@ -1,12 +1,31 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { useCallback } from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+import { useFonts } from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+
+SplashScreen.preventAutoHideAsync()
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    'Baloo2-Regular': require('./assets/fonts/Baloo2-Regular.ttf'),
+    'Baloo2-Bold': require('./assets/fonts/Baloo2-Bold.ttf'),
+    'Baloo2-Medium': require('./assets/fonts/Baloo2-Medium.ttf'),
+  })
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded, fontError])
+
+  if (!fontsLoaded && !fontError) {
+    return null
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Opened 3</Text>
-      <Text style={styles.text2}>Opened 4</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text style={styles.heading}>Baloo2</Text>
+      <Text style={{ fontSize: 30 }}>Platform Default</Text>
     </View>
   )
 }
@@ -14,16 +33,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#27272b',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#27272b',
   },
-  text: {
-    color: 'tomato',
-    fontSize: 25,
-  },
-  text2: {
-    color: 'coral',
-    fontSize: 22,
+  heading: {
+    fontFamily: 'Baloo2-Bold',
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'white',
   },
 })
